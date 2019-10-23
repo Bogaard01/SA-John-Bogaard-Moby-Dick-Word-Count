cb3b6bc6f84fa574f4b7581da4e167df4e1b0ff1
John Bogaard <bogaardj@gmail.com>
2019-10-22 19:34:51 -0500

    Having issues getting Mocha to work.

---
+++ test/Moby Dick/test.js
@@ -0,0 +1,7 @@
+describe("Return from file with array", function() {
+  it("should return arry of words", function() {
+    var test1 = 12;
+    var test2 = 11;
+    return test1 === test2;
+  });
+});
